612b869d7a8d898b38406e20d66dbb2a1fa14ecd
Thales Rodrigues <thaleshend@gmail.com>
2024-02-18 14:49:48 -0300

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 setup(
     name="PyVecs",
-    version="0.0.3",
+    version="0.0.4",
     author="Thales Rodrigues",
     author_email="thaleshend@gmail.com",
     description="A simple 2D and 3D Vector class to Python.",
